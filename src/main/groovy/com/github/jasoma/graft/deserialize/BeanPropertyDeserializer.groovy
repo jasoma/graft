0eca7cffb0af4cd6aa18d6dc714eaa8bc5921508
Jason Maher <jason@jasonmaher.me>
2016-10-26 11:21:38 +0900

    use commons beanutils for now

--- src/main/groovy/com/github/jasoma/graft/deserialize/BeanPropertyDeserializer.groovy
+++ src/main/groovy/com/github/jasoma/graft/deserialize/BeanPropertyDeserializer.groovy
@@ -2,11 +2,13 @@ package com.github.jasoma.graft.deserialize
 
 import com.github.jasoma.graft.access.NeoNode
 import com.github.jasoma.graft.access.ResultRow
+import org.apache.commons.beanutils.PropertyUtils
+
 /**
  * A basic deserializer implementation that directly sets values from the node onto a new instance of the target
  * type using JavaBeans naming conventions. Uses groovy casting via {@code asType} as the only type handling.
  */
-class BeanPropertyDeserializer implements NodeDeserializer, DeserializerUtils {
+class BeanPropertyDeserializer implements NodeDeserializer {
 
     @Override
     def <T> T convert(Class<T> type, NeoNode node, ResultRow row) throws NodeDeserializationException {
@@ -36,14 +38,8 @@ class BeanPropertyDeserializer implements NodeDeserializer, DeserializerUtils {
      * @param row the result row the node came from.
      */
     protected void setProperty(Object instance, String name, Object value, ResultRow row) {
-        def propertyType = getPropertyType(instance, name)
-
-        if (propertyType != null) {
-            instance[name] = value.asType(propertyType)
-        }
-        else {
-            instance[name] = value
-        }
+        def descriptor = PropertyUtils.getPropertyDescriptor(instance, name);
+        instance[name] = value.asType(descriptor.propertyType);
     }
 
 }
